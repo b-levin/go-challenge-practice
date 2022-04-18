4a472adfd2e07534e06611c0cfd9a37c0b9a5842
b-levin <windrider417@gmail.com>
2022-04-18 18:35:32 -0400

    More hashing Qs

--- hashing/hashingQ.go
+++ hashing/hashingQ.go
@@ -1,5 +1,8 @@
 package hashing
 
+import "sort"
+
+//https://practice.geeksforgeeks.org/problems/check-if-two-arrays-are-equal-or-not3847/1
 func ArrEqual(arr1 []int, arr2 []int) int {
 	arr1Map := make(map[int]bool)
 	for _, val := range arr1 {
@@ -11,4 +14,23 @@ func ArrEqual(arr1 []int, arr2 []int) int {
 		}
 	}
 	return 1
+}
+
+//https://practice.geeksforgeeks.org/problems/common-elements5420/1
+func CommonEle(arr1 []int, arr2 []int) []int {
+	out := []int{}
+	//sort arrs
+	sort.Ints(arr1)
+	sort.Ints(arr2)
+	//assuming that array size is equal
+	arr1Map := make(map[int]bool)
+	for _, val := range arr1 {
+		arr1Map[val] = true
+	}
+	for _, val := range arr2 {
+		if _, prs := arr1Map[val]; prs {
+			out = append(out, val)
+		}
+	}
+	return out
 }
--- hashing/hashingQ_test.go
+++ hashing/hashingQ_test.go
@@ -2,6 +2,7 @@ package hashing
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -20,6 +21,27 @@ func TestArrEquals(t *testing.T) {
 		t.Run(testName, func(t *testing.T) {
 			ans := ArrEqual(tt.arr1, tt.arr2)
 			if ans != tt.want {
+				t.Errorf("Got: %d, want: %d", ans, tt.want)
+			}
+		})
+	}
+}
+
+func TestCommonEle(t *testing.T) {
+	var tests = []struct {
+		arr1 []int
+		arr2 []int
+		want []int
+	}{
+		{[]int{3, 4, 2, 2, 4}, []int{3, 2, 2, 7}, []int{2, 2, 3}},
+		{[]int{5, 4, 3, 6, 7, 1, 2}, []int{5, 3, 9, 10, 4, 7, 8}, []int{3, 4, 5, 7}},
+	}
+
+	for _, tt := range tests {
+		testName := fmt.Sprintf("%d, %d", tt.arr1, tt.arr2)
+		t.Run(testName, func(t *testing.T) {
+			ans := CommonEle(tt.arr1, tt.arr2)
+			if !reflect.DeepEqual(ans, tt.want) {
 				t.Errorf("Got: %d, want: %d", ans, tt.want)
 			}
 		})
